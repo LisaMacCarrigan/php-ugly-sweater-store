5306272eb82ef0e62733c9e3a79ac7c8d05c36cf
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2016-11-23 17:18:06 -0500

    update app.php, update composer to account for twig, add basic index view to test site

--- app/app.php
+++ app/app.php
@@ -1,11 +1,25 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Store.php";
+    require_once __DIR__."/../src/Brand.php";
+
     date_default_timezone_set('America/New_York');
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        return "Hi there!";
+    $server = 'mysql:host=localhost:8889;dbname=sweaters';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array ('twig.path' => __DIR__.'/../views'
+    ));
+
+    // use Symfony\Component\HttpFoundation\Request;
+    // Request::enableHttpMethodParameterOverride();
+
+    $app->get("/", function() use($app) {
+        return $app['twig']->render('index.html.twig');
     });
 
     return $app;
