a68efd62843d897d97515127e93066b65549e662
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2016-11-23 22:42:44 -0500

    add passing test for getId function for Store class

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -15,24 +15,23 @@
             Store::deleteAll();
         }
 
-        // function testSave()
-        // {
-        //     // ARRANGE //
-        //     $name = "Sweater Swag";
-        //     $city = "Brooklyn";
-        //     $state = "NY";
-        //     $id = null;
-        //     $test_store = new Store($name, $city, $state, $id);
-        //
-        //     // ACT //
-        //     $test_store->save();
-        //
-        //     // ASSERT //
-        //     $result = Store::getAll();
-        //     $this->assertEquals($test_store, $result[0]);
-        // }
+        function testGetId()
+        {
+            // ARRANGE //
+            $name = "Sweater Swag";
+            $city = "Brooklyn";
+            $state = "NY";
+            $id = 1;
+            $test_store = new Store($name, $city, $state, $id);
 
-        function testGetAll()
+            // ACT //
+            $result = $test_store->getId();
+
+            // ASSERT //
+            $this->assertEquals($id, $result);
+        }
+
+        function testSave()
         {
             // ARRANGE //
             $name = "Sweater Swag";
@@ -40,23 +39,41 @@
             $state = "NY";
             $id = null;
             $test_store = new Store($name, $city, $state, $id);
-            $test_store->save();
-
-            $name = "The Sweat Factory";
-            $city = "San Francisco";
-            $state = "CA";
-            $id = null;
-            $test_store_2 = new Store($name, $city, $state, $id);
-            $test_store_2->save();
 
             // ACT //
+            $test_store->save();
             $result = Store::getAll();
 
             // ASSERT //
-            $this->assertEquals([$test_store, $test_store_2], $result);
+            $this->assertEquals($test_store, $result[0]);
         }
 
         function testDeleteAll()
+        {
+            // ARRANGE //
+            $name_1 = "Sweater Swag";
+            $city_1 = "Brooklyn";
+            $state_1 = "NY";
+            $id = null;
+            $test_store_1 = new Store($name_1, $city_1, $state_1, $id);
+            $test_store_1->save();
+
+            $name_2 = "The Sweat Factory";
+            $city_2 = "San Francisco";
+            $state_2 = "CA";
+            $id = null;
+            $test_store_2 = new Store($name_2, $city_2, $state_2, $id);
+            $test_store_2->save();
+
+            // ACT //
+            Store::deleteAll();
+            $result = Store::getAll();
+
+            // ASSERT //
+            $this->assertEquals([], $result);
+        }
+
+        function testGetAll()
         {
             // ARRANGE //
             $name = "Sweater Swag";
@@ -74,12 +91,12 @@
             $test_store_2->save();
 
             // ACT //
-            Store::deleteAll();
             $result = Store::getAll();
 
             // ASSERT //
-            $this->assertEquals([], $result);
+            $this->assertEquals([$test_store, $test_store_2], $result);
         }
+
     }
 
 ?>
