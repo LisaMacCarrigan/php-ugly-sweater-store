1a6eb91567d5e1b36a59c50c4b13a642498d8584
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2016-11-25 11:30:05 -0500

    add passing test for deleting individual Store objects

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+   *@backupGlobals disabled
+   *@backupStaticAttributes disabled
+   */
+
     require_once "src/Store.php";
     require_once "src/Brand.php";
 
@@ -95,6 +100,30 @@
 
             // ASSERT //
             $this->assertEquals([$test_store, $test_store_2], $result);
+        }
+
+        function testDelete()
+        {
+            // ARRANGE //
+            $name = "Sweater Swag";
+            $city = "Brooklyn";
+            $state = "NY";
+            $id = null;
+            $test_store = new Store($name, $city, $state, $id);
+            $test_store->save();
+
+            $name_2 = "The Sweat Factory";
+            $city_2 = "San Francisco";
+            $state_2 = "CA";
+            $id = null;
+            $test_store_2 = new Store($name_2, $city_2, $state_2, $id);
+            $test_store_2->save();
+
+            // ACT //
+            $test_store->delete();
+
+            // ASSERT //
+            $this->assertEquals([$test_store_2], Store::getAll());
         }
 
     }
